5e17e6382c2856f67bb90d8cc53f261ccbb27f2a
horemast <141536655+horemast@users.noreply.github.com>
2025-08-21 15:32:06 +0800

    Background.vue

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -43,7 +43,7 @@ const changeBg = (type) => {
   } else if (type == 1) {
     bgUrl.value = "https://browser9.qhimg.com/bdm/1600_900_85/t016ad88ddaf2ae2d92.jpg";
   } else if (type == 2) {
-    bgUrl.value = "";
+    bgUrl.value = "https://img.cdn1.vip/i/68a6cb3cb4ef9_1755761468.webp";
   } else if (type == 3) {
     bgUrl.value = "https://img.8845.top/acg/loli2.php";
   }
